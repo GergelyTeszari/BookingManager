e5d8fd96a7c9b60084128876a0a138ae26bc1799
Gergely Teszári <teszari.gergely@gmail.com>
2026-07-05 11:05:00 +0200

    Add portable Windows build scripts

---
+++ build.ps1
@@ -0,0 +1,40 @@
+$vsWhere = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
+
+if (-not (Test-Path $vsWhere))
+{
+    Write-Error "Visual Studio Installer was not found."
+    exit 1
+}
+
+$installationPath = & $vsWhere `
+    -latest `
+    -products * `
+    -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 `
+    -property installationPath
+
+if (-not $installationPath)
+{
+    Write-Error "A Visual Studio installation with C++ build tools was not found."
+    exit 1
+}
+
+$devShellModule = Join-Path `
+    $installationPath `
+    "Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
+
+Import-Module $devShellModule
+Enter-VsDevShell `
+    -VsInstallPath $installationPath `
+    -SkipAutomaticLocation
+
+Set-Location $PSScriptRoot
+
+cl /nologo /EHsc /std:c++20 BookingManager.cpp
+
+if ($LASTEXITCODE -ne 0)
+{
+    Write-Host " ########## Build failed. ##########"
+    exit $LASTEXITCODE
+}
+
+Write-Host " ########## Build successful. ##########"
